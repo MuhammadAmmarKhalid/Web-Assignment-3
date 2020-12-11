c790c24c80c8714d7ac98ec069010ec3d2a7693e
Muhammad Ammar Khalid <ammar.chawla12@gmail.com>
2020-12-11 14:28:28 +0500

    mongo db modified

--- DB/Connection.js
+++ DB/Connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = "mongodb+srv://Chawla:Chawla123@cluster0.8bgku.mongodb.net/ProductDb?retryWrites=true&w=majority";
-
+const URI = "mongodb://Chawla:Chawla123@cluster0-shard-00-00.8bgku.mongodb.net:27017,cluster0-shard-00-01.8bgku.mongodb.net:27017,cluster0-shard-00-02.8bgku.mongodb.net:27017/ProductDb?ssl=true&replicaSet=atlas-d37rx7-shard-0&authSource=admin&retryWrites=true&w=majority";
 const connectDB = async() => {
     await mongoose.connect(URI, {
         useUnifiedTopology:true,
